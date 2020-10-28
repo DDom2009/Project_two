81fc6cd9faa6bc464d35ad749ef2b53a9add57ce
Gerardo Herrera <gerardohe17@gmail.com>
2020-10-27 21:11:42 -0400

    cube movement

---
+++ Project Two/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+     //Start is called before the first frame update
+    float speed = 1f;
+    
+    // Update is called once per frame
+    // Start is called before the first frame update
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.W))
+              transform.position += Vector3.up * speed;
+          //if (Input.GetKey(KeyCode.S))
+              //transform.position += Vector3.down * speed;
+          if (Input.GetKey(KeyCode.A))
+              transform.position += Vector3.left * speed;
+          if (Input.GetKey(KeyCode.D))
+              transform.position += Vector3.right * speed;
+              
+
+    }
+}
+
