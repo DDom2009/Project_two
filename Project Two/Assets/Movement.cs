d56ffafa91bcbaf5842b15bb2068b25708c01216
Mykhal Parson <mykhal.reign@mail.citytech.cuny.edu>
2020-10-30 20:15:37 -0400

    Building Mass & Project Gravity Settings Changed

--- Project Two/Assets/Movement.cs
+++ Project Two/Assets/Movement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
      //Start is called before the first frame update
-    float speed = 1f;
-    
+    float speed = 5f;
+
     // Update is called once per frame
     // Start is called before the first frame update
     void Update()
@@ -19,8 +19,7 @@ public class Movement : MonoBehaviour
               transform.position += Vector3.left * speed;
           if (Input.GetKey(KeyCode.D))
               transform.position += Vector3.right * speed;
-              
+
 
     }
 }
-
