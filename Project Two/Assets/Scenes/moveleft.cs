b98d1f3a20674a78f99599e28420a18a946ba08d
david dominguez <david.dominguez1@mail.citytech.cuny.edu>
2020-11-02 02:45:04 -0500

    move forward code

---
+++ Project Two/Assets/Scenes/moveleft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class moveleft : MonoBehaviour
+{
+
+    public Transform myTransform;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+        speed = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position += Vector3.left * Time.deltaTime * speed;
+
+        if (myTransform.position.x < 8)
+        {
+            myTransform.position += Vector3.right * 16;
+        }
+    }
+}
