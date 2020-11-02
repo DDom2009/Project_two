283ac1a6a8898b3ae4a394ec8fc8a28cdb54dbab
Gerardo Herrera <gerardohe17@gmail.com>
2020-11-02 14:43:06 -0500

    building

---
+++ Project Two/Assets/buildM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildM: MonoBehaviour
+{
+    private Transform myTransform;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+        speed = Random.Range(100f, 210f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position += Vector3.left * Time.deltaTime * speed;
+
+        if (myTransform.position.x < -30)
+        {
+            myTransform.position += Vector3.right * 880;
+        }
+    }
+}
