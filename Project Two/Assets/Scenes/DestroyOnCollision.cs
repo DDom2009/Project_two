de573e7201eb6b00b8135ad5525ba458ec482449
david dominguez <david.dominguez1@mail.citytech.cuny.edu>
2020-11-04 02:11:33 -0500

    destroys cubes on collsion

---
+++ Project Two/Assets/Scenes/DestroyOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collsion)
+    {
+
+        if (collsion .gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
